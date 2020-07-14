f6d83a4ae4ecfff4d4a153a66eb34e54b8e01f33
erol <je.evangelista04@gmail.com>
2020-07-14 14:24:57 -0400

    Added scripts and modified schemas

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -15,7 +15,7 @@ auth = HTTPBasicAuth(USERNAME,PASSWORD)
 with open("../data/libraries.json") as o:
     libraries = json.loads(o.read())
 
-with open("../data/signatures.json") as o:
+with open("../data/signatures_names.json") as o:
     signatures = json.loads(o.read())
 
 
@@ -49,45 +49,109 @@ def empty_cleaner(obj):
     else:
         return obj
 
-res = requests.get(APIURL+"/signatures")
-for s in res.json():
-    r = requests.delete(APIURL+"/signatures/"+s["id"], auth=auth)
-    if not r.ok:
-        print(s["id"])
-        break
+print("deleting signatures...")
+while True:
+    try:
+        res = requests.get(APIURL+"/signatures")
+        for s in res.json():
+            r = requests.delete(APIURL+"/signatures/"+s["id"], auth=auth)
+            time.sleep(0.1)
+            if not r.ok:
+                print(s["id"])
+                raise Exception(r.text)
+        else:
+            break
+    except Exception as identifier:
+        time.sleep(5)        
+        continue
 
-res = requests.get(APIURL+"/libraries")
-for l in res.json():
-    r = requests.delete(APIURL+"/libraries/"+l["id"], auth=auth)
-    if not r.ok:
-        print(l["id"])
 
-failed_libraries = []
-res = requests.get(APIURL+"/libraries")
-libs = [i["id"] for i in res.json()]
-for l in libraries:
-    if l["id"] not in libs:
-        res = requests.post(APIURL+"/libraries/", json=empty_cleaner(l), auth=auth)
-        time.sleep(0.3)
-        if not res.ok:
-            failed_libraries.append(l["id"])
+print("deleting libraries...")
+while True:
+    try:
+        res = requests.get(APIURL+"/libraries")
+        for l in res.json():
+            r = requests.delete(APIURL+"/libraries/"+l["id"], auth=auth)
+            if not r.ok:
+                print(l["id"])
+                raise Exception(r.text)
+        else:
+            break
+    except Exception as identifier:
+        time.sleep(5)        
+        continue
 
-with open("failed_libraries.json", "w") as o:
-    o.write(json.dumps(failed_libraries))
 
-res = requests.get(APIURL+"/signatures")
-sigs = [i["id"] for i in res.json()]
 
-failed_signatures = []
-for l in signatures:
-    if l["id"] not in sigs:
-        res = requests.post(APIURL+"/signatures/", json=empty_cleaner(l), auth=auth)
-        time.sleep(0.3)
-        if not res.ok:
-            failed_signatures.append(l["id"])
+print("adding libraries...")
+while True:
+    try:
+        res = requests.get(APIURL+"/libraries")
+        libs = [i["id"] for i in res.json()]
+        for l in libraries:
+            if l["id"] not in libs:
+                r = requests.post(APIURL+"/libraries/", json=empty_cleaner(l), auth=auth)
+                time.sleep(0.3)
+                if not res.ok:
+                    raise Exception(r.text)
+        else:
+            break
+    except Exception as identifier:
+        time.sleep(5)        
+        continue
 
-with open("failed_signatures.json", "w") as o:
-    o.write(json.dumps(failed_signatures))
 
+print("adding signatures...")
+while True:
+    try:
+        res = requests.get(APIURL+"/signatures")
+        sigs = [i["id"] for i in res.json()]
+        print(len(sigs))
+        for s in signatures:
+            if s["id"] not in sigs:
+                if s["meta"]["KeywordList"] == "[]":
+                    s["meta"]["KeywordList"] = []
+                r = requests.post(APIURL+"/signatures/", json=empty_cleaner(s), auth=auth)
+                time.sleep(0.3)
+                if not r.ok:
+                    raise Exception(r.text)
+        else:
+            break
+    except Exception as identifier:
+        print(identifier)
+        time.sleep(5)        
+        continue
+
+# failed_libraries = []
+# res = requests.get(APIURL+"/libraries")
+# libs = [i["id"] for i in res.json()]
+# for l in libraries:
+#     if l["id"] not in libs:
+#         res = requests.post(APIURL+"/libraries/", json=empty_cleaner(l), auth=auth)
+#         time.sleep(0.3)
+#         if not res.ok:
+#             failed_libraries.append(l["id"])
+
+# with open("failed_libraries.json", "w") as o:
+#     o.write(json.dumps(failed_libraries))
+
+# res = requests.get(APIURL+"/signatures")
+# sigs = [i["id"] for i in res.json()]
+
+# failed_signatures = []
+# for l in signatures:
+#     if l["id"] not in sigs:
+#         res = requests.post(APIURL+"/signatures/", json=empty_cleaner(l), auth=auth)
+#         time.sleep(0.3)
+#         if not res.ok:
+#             failed_signatures.append(l["id"])
+
+# with open("failed_signatures.json", "w") as o:
+#     o.write(json.dumps(failed_signatures))
+
+
+print("refreshing...")
 res = requests.get(APIURL+"/optimize/refresh", auth=auth)
+print(res.ok)
+res = requests.get(APIURL+"/summary/refresh", auth=auth)
 print(res.ok)
---
+++ scripts/patcher.py
@@ -0,0 +1,68 @@
+import os
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+import time
+load_dotenv(dotenv_path="../.env")
+
+APIURL = os.getenv("APIURL")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+auth = HTTPBasicAuth(USERNAME,PASSWORD)
+
+while True:
+    res = requests.get(APIURL+"/signatures")
+    if res.ok:
+        tools = res.json()
+        break
+    else:
+        time.sleep(1)
+with open("../data/patched.txt") as o:
+    patched = set(o.read().split("\n"))
+    print(len(patched))
+    
+with open("../data/patched.txt", "a+") as o:
+    for t in tools:
+        if t["id"] not in patched:
+            if len(patched) % 1000 == 0:
+                print(len(patched))
+            fullname = t["meta"].pop("FullNames", "")
+            year = t["meta"].pop("year", None)
+            if year:
+                t["meta"]["Year"] = str(year)
+            if "Author_Information" in t["meta"]:
+                authors = []
+                for a in t["meta"]["Author_Information"]:
+                    ForeName = a.get("ForeName", "")
+                    LastName = a.get("LastName", "")
+                    Suffix = a.get("Suffix", "")
+                    CollectiveName = a.get("CollectiveName", "")
+                    FullName = ("%s %s %s" % (ForeName, LastName, Suffix)).strip() or CollectiveName
+                    if FullName:
+                        a["Name"] = FullName
+                    authors.append(a)
+                t["meta"]["Author_Information"] = authors
+                t["meta"]["Last_Author"] = authors[-1]
+            c = 0
+            success = False
+            while c<5:
+                try:
+                    r = requests.patch(APIURL+"/signatures/"+t["id"], json=t, auth=auth)
+                    success = True
+                    break
+                except Exception as e:
+                    print("Failed at %s, retrying..."%t["id"])
+                    time.sleep(1)
+                    c+=5
+            if not r.ok and not success:
+                print(res.text)
+                break
+            else:
+                patched.add(t["id"])
+                o.write(t["id"]+"\n")
+
+# with open("../data/patched.txt", "w") as o:
+#     for i in patched:
+#         o.write(i+"\n")
