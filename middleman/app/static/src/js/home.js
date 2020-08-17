81e7dcfa2dcc7830f81f886b281a76ef6dffc5e4
erol <je.evangelista04@gmail.com>
2020-08-16 22:18:08 -0400

    Added pagination

--- middleman/app/static/src/js/home.js
+++ middleman/app/static/src/js/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { fetch_meta_post } from './util/fetch_meta'
 import { get_card_data } from './util/get_card_data'
+import Grid from '@material-ui/core/Grid'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -18,6 +19,8 @@ export default class Home extends React.Component {
             model: Object.keys(this.props.preferred_names)[0],
             index_value: 0,
             expanded: null,
+            page: 0,
+            perPage: 10,
         }
     }
 
@@ -26,7 +29,8 @@ export default class Home extends React.Component {
             endpoint: `/${model}/find`,
             body: {
               filter: {
-                limit: 10,
+                limit,
+                skip
               },
             },
           })
@@ -74,6 +78,34 @@ export default class Home extends React.Component {
         })
     }
 
+    handleChangeRowsPerPage = async (e) => {
+        const {search_results, model} = this.state
+        const {start} = search_results[this.state.model]
+        const perPage = e.target.value
+        search_results[model] = await this.search(model, perPage, start)
+        const coll = this.state.search_results[model].response
+        const collection = this.process_card_data(coll)
+        this.setState({
+            search_results,
+            collection,
+            perPage: perPage
+        })
+    }
+
+    handleChangePage = async (event, page) => {
+        const {search_results, model} = this.state
+        const {start, end} = search_results[this.state.model]
+        const perPage = end-start
+        const skip = page*perPage
+        search_results[model] = await this.search(model, perPage, skip)
+        const coll = this.state.search_results[model].response
+        const collection = this.process_card_data(coll)
+        this.setState({
+            search_results,
+            collection,
+            page
+        })
+    }
     
     render_table = () => {
         if (this.state.collection == null){
@@ -121,32 +153,62 @@ export default class Home extends React.Component {
             return <CircularProgress/>
         }
         return (
-            <div>
-                <Tabs
-                    value={this.state.index_value}
-                    onChange={this.handleChange}
-                    indicatorColor="secondary"
-                    textColor="secondary"
-                    centered
-                    >
-                    {this.state.tabs.map((model) => {
-                        let count_text = ''
-                        let count
-                        if (model !== undefined) {
-                        count = this.state.search_results[model].count || 0
-                        count_text = ` (${count})` 
-                        }
-                        if (count === 0) return null
-                        return (
-                        <Tab
-                            key={model}
-                            label={`${this.props.preferred_names[model]}${count_text}`}
-                        />
-                        )
-                    })}
-                    </Tabs>  
-                    {this.render_table()}
-            </div>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item xs={2}/>
+                <Grid item xs={8}>
+                    <Tabs
+                        value={this.state.index_value}
+                        onChange={this.handleChange}
+                        indicatorColor="secondary"
+                        textColor="secondary"
+                        centered
+                        >
+                        {this.state.tabs.map((model) => {
+                            let count_text = ''
+                            let count
+                            if (model !== undefined) {
+                            count = this.state.search_results[model].count || 0
+                            count_text = ` (${count})` 
+                            }
+                            if (count === 0) return null
+                            return (
+                            <Tab
+                                key={model}
+                                label={`${this.props.preferred_names[model]}${count_text}`}
+                            />
+                            )
+                        })}
+                        </Tabs>  
+                        {this.render_table()}
+                    </Grid>
+                    <Grid item xs={2}/>
+                    <Grid item xs={2}/>
+                    <Grid item xs={8}>
+                        <Grid
+                            container
+                            direction="row"
+                            justify="flex-end"
+                            alignItems="center"
+                        >
+                            <Grid item xs={12}>
+                                <TablePagination
+                                    page={this.state.page}
+                                    rowsPerPage={this.state.perPage}
+                                    count={ this.state.search_results[this.state.model].count}
+                                    onChangePage={(event, page) => this.handleChangePage(event, page)}
+                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                    component="div"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={2}/>
+            </Grid>
         )
     }
 }
