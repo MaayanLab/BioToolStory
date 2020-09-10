64e13e0b8ad3d95383fb4e3beb06efddac444d42
erol <je.evangelista04@gmail.com>
2020-09-10 10:50:14 -0400

    modified add_schema script

--- scripts/add_schemas.py
+++ scripts/add_schemas.py
@@ -15,7 +15,7 @@ APIURL = os.getenv("APIURL")
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
-# auth = HTTPBasicAuth(USERNAME,PASSWORD)
+auth = HTTPBasicAuth(USERNAME,PASSWORD)
 credential = base64.b64encode('{username}:{password}'.format(
     username=USERNAME, password=PASSWORD
   ).encode()).decode()
@@ -40,33 +40,33 @@ def create_schema(meta, validator):
 
 res = requests.get(APIURL+"/schemas")
 for i in res.json():
-    r = requests.delete(APIURL+"/schemas/"+i["id"], headers=headers)
+    r = requests.delete(APIURL+"/schemas/"+i["id"], auth=auth)
 
 with open("../schemas/landing.json") as o:
     landing = json.loads(o.read())
     landing = create_schema(landing, "/dcic/signature-commons-schema/v5/meta/schema/landing-ui.json")
-    r = requests.post(APIURL+"/schemas/", headers=headers, json=landing)
+    r = requests.post(APIURL+"/schemas/", auth=auth, json=landing)
 
 for filename in glob("../schemas/ui-schemas/*.json"):
     with open(filename) as o:
         schema = json.loads(o.read())
-        schema = create_schema(schema, "/dcic/signature-commons-schema/v5/meta/schema/ui-schema.json")
-        r = requests.post(APIURL+"/schemas/", headers=headers, json=schema)
+        schema = create_schema(schema, "https://raw.githubusercontent.com/dcic/signature-commons-schema/v5/meta/schema/ui-schema.json")
+        r = requests.post(APIURL+"/schemas/", auth=auth, json=schema)
 
 with open("../schemas/counts.json") as o:
     counting_meta = json.loads(o.read())
     for i in counting_meta:
         counting_schema = create_schema(i, "/dcic/signature-commons-schema/v5/meta/schema/counting.json")
-        r = requests.post(APIURL+"/schemas/", headers=headers, json=counting_schema)
+        r = requests.post(APIURL+"/schemas/", auth=auth, json=counting_schema)
 
 
-res = requests.get(APIURL+"/optimize/refresh", headers=headers)
+res = requests.get(APIURL+"/optimize/refresh", auth=auth)
 print(res.ok)
 
-res = requests.get(APIURL+"/optimize/status", headers=headers)
-while not res.text == "Ready":
-    time.sleep(1)
-    res = requests.get(APIURL+"/optimize/status", headers=headers)
+# res = requests.get(APIURL+"/optimize/status", auth=auth)
+# while not res.text == "Ready":
+#     time.sleep(1)
+#     res = requests.get(APIURL+"/optimize/status", auth=auth)
 
-res = requests.get(APIURL+"/summary/refresh", headers=headers)
+res = requests.get(APIURL+"/summary/refresh", auth=auth)
 print(res.ok)
